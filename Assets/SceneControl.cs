1c22d2bcf30657c73b5294777a3160091a68a1d5
fufjvnvnf <fufjvnvnf@gmail.com>
2017-02-18 15:33:58 -0500

    finished audio demo

--- Assets/SceneControl.cs
+++ Assets/SceneControl.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class SceneControl : MonoBehaviour {
+
+	[SerializeField]
+	public GameObject person;
 
 	// Use this for initialization
 	void Start () {
 		System.Threading.Thread.Sleep (10000);
-		SceneManager.LoadScene (1);
+//		SceneManager.LoadScene (1);
+		person.GetComponents<AudioSource>()[0].Play();
 	}
 }

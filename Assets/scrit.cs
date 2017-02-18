d791398fd35e1264256793d74263804e041a8427
Tianyi Zhang <tianyizhang@Tianyis-MacBook-Pro.local>
2017-02-17 23:06:37 -0500

    Wall and Camera Script

--- Assets/scrit.cs
+++ Assets/scrit.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class scrit : MonoBehaviour {
 
-	
-	public float smooth = 2.0F;
-	public float tiltAngle = 30.0F;
+	public float turnSpeed = 10f; 
 	void Update() {
-		float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
-		float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
-		Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
-		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+		transform.Rotate (Vector3.up, turnSpeed * Time.deltaTime);
 	}
 }

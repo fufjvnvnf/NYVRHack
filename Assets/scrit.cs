21115871d985c52072e8c563390f55128f181e91
Tianyi Zhang <tianyizhang@Tianyis-MacBook-Pro.local>
2017-02-17 22:56:29 -0500

    Built Town Scene

---
+++ Assets/scrit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrit : MonoBehaviour {
+
+	
+	public float smooth = 2.0F;
+	public float tiltAngle = 30.0F;
+	void Update() {
+		float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+		float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+		Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+	}
+}

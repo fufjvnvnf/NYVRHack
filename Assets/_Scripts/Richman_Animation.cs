67e320aad6c0036100a68b4b4306c0a62601a3f5
Danielqing1998 <DanielQing@Shuhaos-MacBook-Pro.local>
2017-02-18 14:56:44 -0500

    scene_3

--- Assets/_Scripts/Richman_Animation.cs
+++ Assets/_Scripts/Richman_Animation.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Selectable : MonoBehaviour {
+public class Richman_Animation : MonoBehaviour {
+
+	public Animator anim;
+
+
 
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		anim.SetInteger ("MotionSelect", Random.Range (0, 3));
 	}
 }

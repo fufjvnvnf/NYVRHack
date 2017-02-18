798a1465bb75a9daa60cb724630bac6e28345434
fufjvnvnf <fufjvnvnf@gmail.com>
2017-02-18 11:26:49 -0500

    Added switch scene demo

---
+++ Assets/scene_demo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scrit : MonoBehaviour {
+
+	void start() {
+		System.Threading.Thread.Sleep (5000);
+		SceneManager.LoadScene ("Tower_Scene");
+	}
+	void Update() {
+		
+	}
+}
+
+

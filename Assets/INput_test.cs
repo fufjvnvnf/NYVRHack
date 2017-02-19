f38625a167037a8e225a16300fc8fc245c5fc588
Tianyi Zhang <tianyizhang@Tianyis-MacBook-Pro.local>
2017-02-18 22:05:42 -0500

    Throw Scene

---
+++ Assets/INput_test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class INput_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
